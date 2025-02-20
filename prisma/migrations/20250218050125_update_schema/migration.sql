363aea76e2885fde3dd6e62c537d5f673b9c94e1
azibbbbb <azibiqbal01@gmail.com>
2025-02-21 04:28:12 +0800

    Set Prisma to use postgresql

--- prisma/migrations/20250218050125_update_schema/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "ProductColor_productId_idx" ON "ProductColor"("productId");
-
--- CreateIndex
-CREATE INDEX "ProductSize_productColorId_idx" ON "ProductSize"("productColorId");
